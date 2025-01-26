8ab5b931e41a10f1452a653af43ec1f513d2efc8
Sujan Shrestha <sujans@gmeremit.com>
2025-01-26 15:40:57 +0900

    Done rouing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 
-import { createBrowserRouter, RouterProvider,HashRouter, Routes, Route } from 'react-router-dom';
+import { createHashRouter, RouterProvider,HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
@@ -10,7 +10,7 @@ import MovieDetails from './components/MovieDetails/MovieDetails';
 import NotFound from './components/NotFound/NotFound';
 import {ThemeProvider} from './provider/ThemeProvider';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Home />,
